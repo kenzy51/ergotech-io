7b010f29730a7282bfcba54b4c74dde753d4e489
Kenzy2112 <123618123+kenzy51@users.noreply.github.com>
2024-01-25 20:23:54 +0600

    added contact info

--- src/widgets/QRSection/ui/QR.tsx
+++ src/widgets/QRSection/ui/QR.tsx
@@ -1,45 +1,49 @@
-import { theme } from '@/styles/theme';
-import React from 'react';
-import QRCode from 'react-qr-code';
-import { styled } from 'styled-components';
+import { theme } from "@/styles/theme";
+import React from "react";
+import QRCode from "react-qr-code";
+import { styled } from "styled-components";
 
 const QR = () => {
-    return (
-        <Wrapper>
-            <Container>
-                <QRCode
-                    size={156}
-                    style={{ height: "auto", maxWidth: "30%", width: "100%" }}
-                    value={'https://t.me/Aibek87    '}
-                    viewBox={`0 0 256 256`}
-                />
-                <h1>Our app is excited to be tested </h1>
-            </Container>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Container>
+        <ContactInfo>
+          <h1>ErgoTech LLC</h1>
+          <a href="tel:+16186666119">+1 618-666-6119</a>
+          <a href="mailto:eric@ergotech.io">Email: eric@ergotech.io</a>
+          <address>Address: 35 N Kukui St #411, Honolulu HI 96817</address>
+        </ContactInfo>
+      </Container>
+    </Wrapper>
+  );
 };
 
+const ContactInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+`;
+
 const Wrapper = styled.div`
-     color: white;
-     display: flex;
-     justify-content:space-between;
+  color: white;
+  display: flex;
+  justify-content: space-between;
   background: #212b33;
   padding: 100px 0;
   height: auto;
-  font-family: 'Plus Jakarta Sans', sans-serif;
-  ${theme.mqMax('lg')} {
+  font-family: "Plus Jakarta Sans", sans-serif;
+  ${theme.mqMax("lg")} {
     height: auto;
     padding-bottom: 30px;
   }
-
-`
+`;
 const Container = styled.div`
   display: flex;
   gap: 3rem;
   max-width: ${theme.containers.main};
   margin: 0 auto;
   width: 100%;
-  ${theme.mqMax('lg')} {
+  ${theme.mqMax("lg")} {
     padding-left: 35px;
     padding-right: 35px;
     flex-direction: column-reverse;
